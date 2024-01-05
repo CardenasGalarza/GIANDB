1d7b8583697970b62b8dd5541242051168b6e950
CardenasGalarza <alex10estadistica@gmail.com>
2024-01-05 10:03:11 -0500

    fff

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='giandb',
+    name='ESPEROOK',
     version='0.0.2',  # Cambia la versión a la nueva
     license='MIT',
     description="paquete gian de insertar data dba",
@@ -9,7 +9,7 @@ setup(
     install_requires=['numpy'],
     author_email="alex10estadistica@gmail.com",
     packages=find_packages(),
-    url="https://github.com/CardenasGalarza/GIANDB",
+    url="https://github.com/CardenasGalarza/ESPEROOK",
     entry_points={
         'console_scripts': [
             'dbgian = mimodulo:main',  # Reemplaza 'mimodulo' con el nombre real de tu módulo y 'main' con la función principal si la tienes
